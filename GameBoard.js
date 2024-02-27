026c5e7bd6a46431191b38cfac5d8d8a921e5de7
Kauthar <kauthar.hussaini@ada.ac.uk>
2024-02-27 18:52:16 +0000

    Game board design and scaffolding

--- GameBoard.js
+++ GameBoard.js
@@ -18,5 +18,34 @@ class GameBoard {
         this.dotCount = 0;
         this.grid = [];
         this.DOMGrid.innerHTML = '';
+        this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`;
 
-    } }}
+        level.forEach((square) => {
+            const div = document.createElement('div');
+            div.classList.add('square', CLASS_LIST[square]);
+            div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px;`;
+            this.DOMGrid.appendChild(div);
+            this.grid.push(div);
+
+            if (CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
+
+          })
+
+        }
+        addObject(pos, classes) {
+            this.grid[pos].classList.add(...classes);
+        }
+
+        removeObject(pos, classes) {
+            this.grid[pos].classList.remove(...classes);
+
+        }
+
+        objectExists(pos, object) { 
+            return this.grid[pos].classList.contains(object);
+        }
+
+        rotateDiv(pos, deg) {
+            this.grid[pos].style.transform = `rotate(${deg}deg)`;
+        }
+    }
